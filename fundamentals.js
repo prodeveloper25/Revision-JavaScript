4751d03e535f7cae5a687be1dc009e2eb9668510
prodeveloper25 <mdnahid.prg@gmail.com>
2024-12-20 20:36:51 +0600

    Declare 2 of Variable Using Let and Const and Practice Condition and Multiple Condition

---
+++ fundamentals.js
@@ -0,0 +1,14 @@
+// 1. how to declare a variable using let and const
+const fatherNames = "Kazi Mohamed Mosharop Hossain";
+let season = "winter";
+season = "reiny";
+
+// 2. Condition >,<,===,!=,<=,>=
+// 2.1 Multiple Condition &&, ||
+if (fatherNames === "Kazi Mohamed Mosharop Hossain" || season === "reiny") {
+  console.log("If is Win");
+} else if (fatherNames === "kazi mohamed mosharop hossain") {
+  console.log("else if is win");
+} else {
+  console.log("else is win");
+}
