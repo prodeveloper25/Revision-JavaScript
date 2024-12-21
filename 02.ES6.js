fb940129fe847c380fc41d37ef113ab3d02a477b
prodeveloper25 <mdnahid.prg@gmail.com>
2024-12-21 16:16:57 +0600

    The ES6.JS Is done

--- 02.ES6.js
+++ 02.ES6.js
@@ -4,5 +4,42 @@ const student = {
   age: 17,
   designation: ["Programmer", "Reels Manager"],
 };
-const about = `My name is ${student.name} age of ${student["age"]}`;
+
+/* 1. Template String */
+const about = `My name is ${student.name} age of ${student["age"]} has number ${numbers[2]} also liked designation ${student.designation[0]}`;
+console.log("1. Template String");
 console.log(about);
+
+/* 2. Arrow Function */
+const nahid = () => "is powerful";
+const sumWithANumber = (num) => num + 23;
+const isEven = (x) => x % 2 == 1;
+const addThree = (x, y, z) => x + y + z;
+const doMath = (num1, num2) => {
+  const sum = num1 + num2;
+  return sum;
+};
+const firstAndLastName = (firstname, lastname) => {
+  const fullName = firstname + " " + lastname;
+  return fullName;
+};
+console.log("2. Arrow Function");
+console.log(nahid());
+console.log(sumWithANumber(23));
+console.log(isEven(24));
+console.log(addThree(2, 5, 3));
+console.log(doMath(6, 6));
+console.log(firstAndLastName("Nahid", "Hasan"));
+
+/* 3. Spreed Operation */
+// 3.1 Spreed Explain: spreed operation diye amra akti array ke clone ba copy korte pari
+const mainArray = [45, 21, 83, 51, 95];
+const newMainArray = [...mainArray];
+// create a new array from an older array and add an element
+const currentMainArray = [...mainArray, 96];
+mainArray.push(33);
+newMainArray.shift();
+console.log("3. Spreed Operation");
+console.log("Main Array", mainArray);
+console.log("New Main Array", newMainArray);
+console.log("Current Main Array", currentMainArray);
